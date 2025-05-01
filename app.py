8f1c567726b098ab00193062ea49b71a83fc6d9c
lachlan-cooney <lachlan7651@gmail.com>
2025-05-01 21:57:24 +0100

    feat: created function to insert new posts into database via POST method

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def get_post(post_id):
 
 
 app = Flask(__name__)
-app.config['TEST_KEY'] = KEY
+app.config['SECRET_KEY'] = KEY
 
 
 @app.route('/')
@@ -44,6 +44,22 @@ def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
 
+
 @app.route('/create', methods=('GET', 'POST'))
 def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_conn()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
     return render_template('create.html')
+
